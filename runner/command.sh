3949f9d43eadd139a54b01203bedbd640a3eb401
zuker <lndkuzmin@gmail.com>
2016-09-07 20:12:18 +0300

    removed echo

--- runner/command.sh
+++ runner/command.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 mem_limit_bytes=`cat /sys/fs/cgroup/memory/memory.limit_in_bytes`
 mem_limit_Mbytes=`expr $mem_limit_bytes / 1048576`
-echo $mem_limit_bytes
-echo $mem_limit_Mbytes
+
 node () {
     `which node` --max-old-space-size=$mem_limit_Mbytes $@
 }
