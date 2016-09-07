f59e40703bea19eb322a8af7302ac4454a931180
zuker <lndkuzmin@gmail.com>
2016-09-07 20:11:16 +0300

    node max old space size from cgroup limits

--- runner/command.sh
+++ runner/command.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
+mem_limit_bytes=`cat /sys/fs/cgroup/memory/memory.limit_in_bytes`
+mem_limit_Mbytes=`expr $mem_limit_bytes / 1048576`
+echo $mem_limit_bytes
+echo $mem_limit_Mbytes
 node () {
-    `which node` --max-old-space-size=$COMP_MEM_LIMIT_MB $@
+    `which node` --max-old-space-size=$mem_limit_Mbytes $@
 }
 $command
